3b3cad068d9b29a647ad5607f7bfae668c37938f
동협 갈 <bluff48265@gmail.com>
2022-08-17 21:45:38 +0900

    (반복문)22.8.17

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,134 +1,91 @@
 #include <stdio.h>
+
 void main()
-{	
-	// 사분면
-	// 제 1 사분면 (+x,+y)
-	// 제 2 사분면 (-x,+y)
-	// 제 3 사분면 (-x,-y)
-	// 제 4 사분면 (+x,-y)
-	// y 절편 : (x[0], y)
-	// x 절편 : (x, y[0])
-	// 원점 : (x[0], y[0])
+{
 	
-	int x = 0;
-	int y = 0;
-
-	if (x > 0 && y > 0)
-	{
-		printf("제 1 사분면 입니다.");
-	}
-	else if (x < 0 && y > 0)
-	{
-		printf("제 2 사분면 입니다.");
-	}
-	else if (x < 0 && y < 0)
-	{
-		printf("제 3 사분면 입니다.");
-	}
-	else if (x > 0 && y < 0)
-	{
-		printf("제 4 사분면 입니다.");
-	}
-	else if (x != 0 && y == 0)
-	{
-		printf("x 절편");
-	}
-	else if (x == 0 && y != 0)
-	{
-		printf("y 절편");
-	}
-	else if (x == 0 && y == 0)
-	{
-		printf("원점");
-	}
-	
-	
-	
-	
-	// != -> 같지않다
-	
-	// 논리 연산자
-	// 1 = 참
-	// 0 = 거짓
-	
-	// && (AND) : 두 개의 조건 모두 참이라면 조건을 실행
-	// || (OR) : 두 개의 조건에 하나라도 참이면 조건을 실행
+	// 반복문
+	// 프로그램 내에서 특정한 작업을 반복적으로	수행하는 명령문
 
 	
-	if (x == 10 && y == 20)
-	{
-		printf("논리 연산 AND가 참입니다.\n");
+	// 증감 연산자
+	/*
+	// 피연산자를 하나씩 증가시키거나 감소시킬 때 사용하는 연산자
 
-		if (5 < 10)
+	// 전위 (증가 감소) 문자 앞에 ++ or --
+	// 변수의 값을 증감시킨 후에 연산을 수행
+
+	int value = 0;
+
+	int result = ++value;
+	
+	printf("value 변수의 전위 증가한 값 : %d\n", result);
+
+
+
+
+	// 후위 (증가,감소) 문자 뒤에 ++ or --
+	// 연산을 수행한 후에 변수의 값을 증가시킨다
+
+	result = value++;
+	printf("value 변수의 후위 증가한 값 : %d\n", result);
+	printf("value의 결과 : %d", result);
+	*/
+
+	// for 문
+	/*
+	// 초기식을 연산하여 조건식의 결과에 따라 특정한 횟수만큼 반복하는 반복문
+
+	// 1. 초기식 (i = 0)
+	// 2. 조건식 (i가 5보다 작을 때)
+	// 3. 실행하고 싶은 명령문
+	// 4. 증감식
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("안녕하세요!?\n");
+	}
+	
+	
+	// for문을 이용해서 순차적으로 증가하는 초기식의 값을 출력
+	// 1 ~ 5 ,5 ~ 1
+
+	for (int a = 5; a > 0; a--)
+	{
+		printf("%d\n", a);
+		
+
+	// 약수
+	// 어떤 수를 나누어 떨어지게하는 수
+
+	int value = 6;
+	for (int b = 1; b <= value; b++)
+	{
+		if (value % b == 0)
 		{
-			printf("if문 안에 있는 조건문입니다.\n");
+			printf("%d ", b);
 		}
 
-
 	}
+	*/
 
-	if (x == 10 || y == 25)
+	// 이중 for 문
+	/*
+	for (int i = 0; i < 3; i++)
 	{
-		printf("논리 연산자 OR이 참입니다.\n ");
+		for (int j = 0; j < 3; j++)
+		{
+			printf("안녕하세요\n");
+		}
 	}
+	*/
 
-	// switch 문
-	// 어떤 결과에 따라 그 결과부터 실행되는 조건문
-
-	// break 문
-	// 특정한 시점에서 분기를 탈출하는 제어문
-
-	// default
-	// case에 없는 값이 들어올경우 default로 빠짐
-
-	int value = 5;
-	switch (value)
+	for (int i = 0; i < 3; i++)
 	{
-        case 0: printf("0 입니다\n");
-			break;
-		
-		case 1: printf("1 입니다\n");
-			break;
-		default: printf("잘못 선택했습니다.\n");
+		printf("주황색\n");
+		for (int j = 0; j < 3; j++)
+		{
+			printf("검은색\n");
+		}
 	}
-	
-	
-	// 대학 성적표
-	// A는 100 ~ 90 점
-	// B는 90 ~ 80 점
-	// C는 80 ~ 70 점
-	// D는 70 ~ 60 점
-	// E는 60 ~ 50 점
-	// 과락입니다.
-
-	char alphabet = 'D';
-
-	switch (alphabet)
-	{
-	case 'A': printf("100 ~ 90점 입니다");
-		break;
-	case 'B': printf("90 ~ 80점 입니다");
-		break;
-	case 'C': printf("80 ~ 70점 입니다");
-		break;
-	case 'D': printf("70 ~ 60점 입니다");
-		break;
-	case 'E': printf("60 ~ 50점 입니다");
-		break;
-	default: printf("과락입니다");
-	}
-	
-
-    float variabel = 3.25;
-	
-	switch (variable)
-	{
-	  case 3.25: printf("부동 소수점의 오차로 인해 switch의 조건을 사용할 수 없습니다.")
-	}
-
-
-
-
-
-
 }
